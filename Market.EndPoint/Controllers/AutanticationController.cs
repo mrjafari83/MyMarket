f2e26cbe0c358e97cda809bd8368e76191a7a56b
Mohammad Jafari <mr.m.jafari.83@gmail.com>
2022-04-25 12:07:48 +0430

    I implemented the reset password view and controller. also implement edt user info view

--- Market.EndPoint/Controllers/AutanticationController.cs
+++ Market.EndPoint/Controllers/AutanticationController.cs
@@ -18,7 +18,7 @@ namespace Market.EndPoint.Controllers
         private readonly IMessageSender messageSender;
         private readonly IClientCartFacad _clientCartFacad;
         public AutanticationController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager
-            ,IMessageSender messageSender , IClientCartFacad clientCartFacad)
+            , IMessageSender messageSender, IClientCartFacad clientCartFacad)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
@@ -36,28 +36,27 @@ namespace Market.EndPoint.Controllers
 
         [Route("Register")]
         [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel user , string returnUrl = "")
+        public async Task<IActionResult> Register(RegisterViewModel user, string returnUrl = "")
         {
             var newUser = new IdentityUser()
             {
                 Email = user.Email,
                 UserName = user.UserName,
             };
-
-            var result = await userManager.CreateAsync(newUser , user.Password);
+            var result = await userManager.CreateAsync(newUser, user.Password);
 
             if (result.Succeeded)
             {
                 //var confirmationEmailToken = userManager.GenerateEmailConfirmationTokenAsync(newUser);
                 //var emailMessage = Url.Action("ConfirmEmail", "Autantication" ,
-                    //new { username = newUser.UserName, token = confirmationEmailToken } , Request.Scheme);
+                //new { username = newUser.UserName, token = confirmationEmailToken } , Request.Scheme);
                 //await messageSender.SendAsync(user.Email, "تایید ایمیل", emailMessage, false);
 
                 await signInManager.PasswordSignInAsync(user.UserName, user.Password, false, true);
             }
 
             int cartId;
-            _clientCartFacad.CreateCart.Execute(user.UserName , out cartId);
+            _clientCartFacad.CreateCart.Execute(user.UserName, out cartId);
             CookiesManager.AddCookie(HttpContext, "CartId", cartId.ToString());
 
 
@@ -110,21 +109,52 @@ namespace Market.EndPoint.Controllers
         {
             await signInManager.SignOutAsync();
             if (returnUrl != "" && Url.IsLocalUrl(returnUrl))
-                    return Redirect(returnUrl);
-                return Redirect("/");
+                return Redirect(returnUrl);
+            return Redirect("/");
         }
 
-        public async Task<IActionResult> ConfirmEmail(string userName , string token)
+        public async Task<IActionResult> ConfirmEmail(string userName, string token)
         {
             if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(token))
                 return Redirect("/");
 
             var user = await userManager.FindByNameAsync(userName);
-            if(user == null)
+            if (user == null)
                 return Redirect("/");
 
             await userManager.ConfirmEmailAsync(user, token);
             return Redirect("/");
+        }
+
+        [Route("Dashboard")]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [Route("EditUser")]
+        public IActionResult EditUserInfo()
+        {
+            return View();
+        }
+
+        [Route("RestPassword")]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [Route("RestPassword")]
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(string currentPassword = "", string newPassword = "")
+        {
+            var user = userManager.FindByNameAsync(User.Identity.Name).Result;
+            var result = await userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+
+            if (result.Succeeded)
+                return Redirect("/Dashboard");
+
+            return Redirect("/RestPassword");
         }
     }
 }
