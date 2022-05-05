05235338a33234f30e880e72b30071f70f8ccdcf
Mohammad Jafari <mr.m.jafari.83@gmail.com>
2022-05-05 06:45:26 +0430

    I edit header of website and create last blog pages in site footter

---
+++ Market.EndPoint/ViewComponents/LastBlogPagesInFotter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Interfaces.FacadPatterns.Common;
+
+namespace Market.EndPoint.ViewComponents
+{
+    public class LastBlogPagesInFotter : ViewComponent
+    {
+        private readonly ICommonBlogPageFacad _commonBlogPageFacad;
+        public LastBlogPagesInFotter(ICommonBlogPageFacad commonBlogPageFacad)
+        {
+            _commonBlogPageFacad = commonBlogPageFacad;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("LastBlogPagesInFotter" , _commonBlogPageFacad.GetAllBlogPages.Execute(Common.Enums.Enums.PagesFilter.Newest ,1 , 2).Data);
+        }
+    }
+}
