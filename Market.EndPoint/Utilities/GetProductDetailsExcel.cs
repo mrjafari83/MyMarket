61d76fe76a310e34af8d22f850ca2465123dae9f
Mohammad.Jafari <mr.m.jafari.83@gamil.com>
2022-06-26 09:35:47 +0430

    I set customization of excel files.

--- Market.EndPoint/Utilities/GetProductDetailsExcel.cs
+++ Market.EndPoint/Utilities/GetProductDetailsExcel.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Drawing;
 
 namespace Market.EndPoint.Utilities
 {
@@ -27,12 +29,23 @@ namespace Market.EndPoint.Utilities
                 excel.Workbook.Worksheets.Add("محصولات");
 
                 var excelWorkSheet = excel.Workbook.Worksheets["محصولات"];
+                excelWorkSheet.View.RightToLeft = true;
+
                 List<string[]> header = new List<string[]>
                 {
                       new string[]{"نام","نام دسته بندی","برند","رنگ","سایز","تعداد بازدید","تعداد فروخته شده","قیمت","موجودی"}
                  };
                 string headerRange = "A1:" + Char.ConvertFromUtf32(header[0].Length + 64) + "1";
                 excelWorkSheet.Cells[headerRange].LoadFromArrays(header);
+                excelWorkSheet.Column(1).Width = 20;
+                excelWorkSheet.Column(2).Width = 25;
+                excelWorkSheet.Column(3).Width = 20;
+                excelWorkSheet.Column(4).Width = 20;
+                excelWorkSheet.Column(5).Width = 20;
+                excelWorkSheet.Column(6).Width = 15;
+                excelWorkSheet.Column(7).Width = 20;
+                excelWorkSheet.Column(8).Width = 25;
+                excelWorkSheet.Column(9).Width = 15;
 
                 List<string[]> data = new List<string[]>();
                 var products = await _optionFacade.GetAllProductDetailsService.Execute(Ids);
@@ -49,6 +62,40 @@ namespace Market.EndPoint.Utilities
                         product.Price.ToString(),
                         product.Inventory.ToString(),
                     });
+
+                for(int i = 1;i <= data.Count+1; i++)
+                {
+                    if(i == 1)
+                    {
+                        for(int j = 1; j <= data.FirstOrDefault().Length; j++)
+                        {
+                            ExcelRange range = excelWorkSheet.Cells[i,j];
+                            range.Style.Font.SetFromFont("B Yekan",15);
+                            range.Style.Font.Color.SetColor(1, 7, 16, 177);
+                            range.Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                            range.Style.Border.Bottom.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                            range.Style.Border.Left.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                            range.Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                            range.Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                            range.Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 1; j <= data.FirstOrDefault().Length; j++)
+                        {
+                            ExcelRange range = excelWorkSheet.Cells[i, j];
+                            range.Style.Font.SetFromFont("B Yekan", 12 , false);
+                            range.Style.Font.Color.SetColor(1, 0, 0, 0);
+                            range.Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                            range.Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                            range.Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                            range.Style.Border.Bottom.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                            range.Style.Border.Left.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                            range.Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                        }
+                    }
+                }
 
                 excelWorkSheet.Cells[2, 1].LoadFromArrays(data);
                 string folderAddress = _environment.WebRootPath + $@"/Excels/";
